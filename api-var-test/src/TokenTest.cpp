063454955fd7b2e71cafa1be6a7bc7bd1b4c80b8
Shoma Gane <shomagan@gmail.com>
2018-04-08 22:55:46 +0500

    add api-var-test for token 		first step

--- api-var-test/src/TokenTest.cpp
+++ api-var-test/src/TokenTest.cpp
@@ -1,3 +1,4 @@
+#include <sapi/var.hpp>
 #include "TokenTest.hpp"
 
 TokenTest::TokenTest(): Test("var::Token"){
@@ -9,16 +10,51 @@ TokenTest::TokenTest(): Test("var::Token"){
 
 bool TokenTest::execute_class_performance_case(){
     bool result = true;
+
     return result;
 }
 
 bool TokenTest::execute_class_stress_case(){
     bool result = true;
+
     return result;
 }
 
 bool TokenTest::execute_class_api_case(){
     bool result = true;
+    String s1 = "Uno,dos,tres, quatro or  cinko";
+    Token one(s1.c_str(),",. ",";",0);
+    if(one.size() != 6){
+        print_case_message("token", "token calculate failed");
+        result = false;
+    }
+    String s[8];
+    s[0] = "uno";
+    s[1] = "dos";
+    s[2] = "tres";
+    s[3] = "quatro";
+    s[4] = "cinko";
+    s[5] = "seis";
+    s[6] = "sieta";
+    s[7] = "ocho";
+    String s_common;
+    for (u32 i =0;i<48;i++){
+        s_common.append(s[i%8].c_str());
+        if(i<16){
+            s_common.append(",");
+        }else if(i<32){
+            s_common.append(";");
+        }else if(i<47){
+            s_common.append(" ");
+        }
+    }
+    Token two(s_common.c_str(),",; ","(",0);
+    for(u32 i =0;i<48;i++){
+        if (strncmp(two.at(i),s[i%8],s[i%8].len())){
+            result = false;
+        }
+    }
+
     return result;
 }
 
