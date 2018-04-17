1c1845380096ac2575409e75eabb71747e0a8ce1
Shoma Gane <shomagan@gmail.com>
2018-04-17 18:30:46 +0500

    add stress test for Array to api-var-test

--- api-var-test/src/ArrayTest.hpp
+++ api-var-test/src/ArrayTest.hpp
@@ -12,7 +12,8 @@ private:
     bool execute_class_api_case();
     bool execute_class_performance_case();
     bool execute_class_stress_case();
-
+    bool execute_recursive(Array <u64,64>array);
+    static int recursive_number;
 };
 
 #endif // TOKENTEST_HPP
