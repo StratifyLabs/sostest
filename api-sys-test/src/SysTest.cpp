7fc345015fd854178bad4cbe48a1a5bcd25e3256
Shoma Gane <shomagan@gmail.com>
2018-06-23 00:21:47 +0500

    add api and stress test for sys::Sys

--- api-sys-test/src/SysTest.cpp
+++ api-sys-test/src/SysTest.cpp
@@ -1,41 +1,127 @@
 
 #include <sapi/sys.hpp>
 #include <sapi/chrono.hpp>
+#include <sapi/var.hpp>
+#include <sapi/hal.hpp>
 #include "SysTest.hpp"
-
+static void rand_string_value(u16 size,String & string);
 SysTest::SysTest() : Test("sys::Test"){
 
 }
 /*@brief api test for sys/Sys use "api-sys-test -sys -api"
  * Sys
  * launch,free_ram
+ * assign_zero_sum32,verify_zero_sum32,
+ * open,get_version,get_kernel_version,get_board_config,
+ * get_info,get_23_info,get_26_info,get_taskattr,current_task,
+ * set_current_task,get_id,
  * not writed test
- * ,reclaim_ram,assign_zero_sum32,verify_zero_sum32,
- * get_version,get_kernel_version,powerdown,hibernate,request,reset,
- * get_board_config,
- * open,get_info,get_23_info,get_26_info,get_taskattr,current_task,
- * set_current_task,get_id,redirect_stdout,redirect_stdin,redirect_stderr,
- *
+ * reclaim_ram,
+ * ,powerdown,hibernate,request,reset,
+ * redirect_stdout,redirect_stdin,redirect_stderr,
  * not tested on
  *
  */
 bool SysTest::execute_class_api_case(){
     bool result;
+    String version;
     result = true;
+    const u16 buff_size = 50*sizeof(u32);
+    u8 temp_buff[buff_size];
+    sos_board_config_t config;
+    sys_info_t sys_info;
+    sys_23_info_t sys_23_info;
+    sys_26_info_t sys_26_info;
+    Sys board;
     //start fake
     if(Sys::launch("/app/flash/fake",0,"orphan",0,Sys::LAUNCH_RAM_SIZE_DEFAULT,0,0)>=0){
         print_case_message("Failed %s:%d", __PRETTY_FUNCTION__, __LINE__);
         result = false;
     }
     //start preinstalled app launchslave
-    if(Sys::launch("/app/flash/launchslave",0,"orphan",0,Sys::LAUNCH_RAM_SIZE_DEFAULT,0,0)<0){
+    if(Sys::launch("/app/flash/launchslave",0,"task_id",0,Sys::LAUNCH_RAM_SIZE_DEFAULT,0,0)<0){
+        print_case_message("need to install launchslave app");
         print_case_message("Failed %s:%d", __PRETTY_FUNCTION__, __LINE__);
         result = false;
     }
-    Timer::wait_microseconds(10000);
+    Timer::wait_microseconds(40000);
     if(Sys::free_ram("/app/flash/launchslave",0)!=0){
         print_case_message("Failed %s:%d", __PRETTY_FUNCTION__, __LINE__);
         result = false;
+    }
+    for (u32 i =0;i<buff_size;i++){
+       temp_buff[i] = i;
+    }
+    Sys::assign_zero_sum32(temp_buff,buff_size);
+    if(!Sys::verify_zero_sum32(temp_buff,buff_size)){
+        print_case_message("Failed %s:%d", __PRETTY_FUNCTION__, __LINE__);
+        result = false;
+    }
+    if(Sys::get_version(version)!=0){
+        print_case_message("Failed %s:%d", __PRETTY_FUNCTION__, __LINE__);
+        result = false;
+    }
+    print_case_message("version %s",version.str());
+    if(Sys::get_kernel_version(version)!=0){
+        print_case_message("Failed %s:%d", __PRETTY_FUNCTION__, __LINE__);
+        result = false;
+    }
+    print_case_message("kernel version %s",version.str());
+    if(board.open()!=0){
+        print_case_message("Failed %s:%d", __PRETTY_FUNCTION__, __LINE__);
+        result = false;
+    }
+    if(board.get_board_config(config)!=0){
+        print_case_message("Failed %s:%d", __PRETTY_FUNCTION__, __LINE__);
+        result = false;
+    }else{
+        print_case_message("config %s",config.sys_version);
+    }
+    if(board.get_info(sys_info)!=0){
+        print_case_message("Failed %s:%d", __PRETTY_FUNCTION__, __LINE__);
+        result = false;
+    }else{
+        print_case_message("sys_info %s",sys_info.name);
+    }
+    if(board.get_23_info(sys_23_info)!=0){
+        print_case_message("Failed %s:%d", __PRETTY_FUNCTION__, __LINE__);
+        result = false;
+    }else{
+        print_case_message("sys_23_info %s",sys_23_info.name);
+    }
+    if(board.get_26_info(sys_26_info)!=0){
+        print_case_message("Failed %s:%d", __PRETTY_FUNCTION__, __LINE__);
+        result = false;
+    }else{
+        print_case_message("sys_26_info %s",sys_26_info.name);
+    }
+    sys_taskattr_t task_attr;
+    if(board.get_taskattr(task_attr)!=0){
+        print_case_message("Failed %s:%d", __PRETTY_FUNCTION__, __LINE__);
+        result = false;
+    }else{
+        print_case_message("task_attr %s",task_attr.name);
+    }
+    int current_task;
+    current_task = board.current_task();
+    if(current_task<=0){
+        print_case_message("Failed %s:%d", __PRETTY_FUNCTION__, __LINE__);
+        result = false;
+    }else{
+        print_case_message("current task %d",current_task);
+    }
+    current_task+=1;
+    board.set_current_task(current_task);
+    if(board.current_task() != current_task){
+        print_case_message("Failed %s:%d", __PRETTY_FUNCTION__, __LINE__);
+        result = false;
+    }
+    sys_id_t sys_id;
+    if(board.get_id(sys_id)!=0){
+        print_case_message("Failed %s:%d", __PRETTY_FUNCTION__, __LINE__);
+        result = false;
+    }else{
+        print_case_message("sys_id %s",sys_id.id);
     }
     return result;
 }
@@ -48,13 +134,76 @@ bool SysTest::execute_class_api_case(){
 bool SysTest::execute_class_stress_case(){
     bool result;
     result = true;
-    const u32 itterate_num = 100;
+    char exec_path[512];
+    char arg_buff[48];
+    String temp_str;
+    const u32 itterate_num = 64;
+    u16 ram_size;
+    int pid;
     for(u32 i=0;i<itterate_num;i++){
         //launch apps
+        //start preinstalled app launchslave
+        rand_string_value(sizeof(arg_buff),temp_str);
+        ram_size = rand() & 0xfff;
+        pid = Sys::launch("/app/flash/launchslave",exec_path,arg_buff,\
+                          Sys::LAUNCH_OPTIONS_FLASH, ram_size ,0,0);
+        if(pid < 0){
+            print_case_message("Failed %s:%d", __PRETTY_FUNCTION__, __LINE__);
+            result = false;
+            break;
+        }
+        //print_case_message("LAUNCH_OPTIONS_FLASH %d",pid);
+        Timer::wait_microseconds(40000);
+        if(Sys::free_ram("/app/flash/launchslave",0)!=0){
+            print_case_message("Failed %s:%d", __PRETTY_FUNCTION__, __LINE__);
+            result = false;
+            break;
+        }
+        pid = Sys::launch("/app/flash/launchslave",exec_path,arg_buff,\
+                          Sys::LAUNCH_OPTIONS_ROOT, ram_size ,0,0);
+        if(pid<0){
+            print_case_message("Failed %s:%d", __PRETTY_FUNCTION__, __LINE__);
+            result = false;
+            break;
+        }
+        //print_case_message("LAUNCH_OPTIONS_ROOT %d",pid);
+        Timer::wait_microseconds(40000);
+        if(Sys::free_ram("/app/flash/launchslave",0)!=0){
+            print_case_message("Failed %s:%d", __PRETTY_FUNCTION__, __LINE__);
+            result = false;
+            break;
+        }
+        pid = Sys::launch("/app/flash/launchslave",exec_path,arg_buff,\
+                          Sys::LAUNCH_OPTIONS_ORPHAN, ram_size ,0,0);
+        if(pid < 0){
+            print_case_message("Failed %s:%d", __PRETTY_FUNCTION__, __LINE__);
+            result = false;
+            break;
+        }
+        //print_case_message("LAUNCH_OPTIONS_ORPHAN %d",pid);
+        Timer::wait_microseconds(40000);
+        if(Sys::free_ram("/app/flash/launchslave",0)!=0){
+            print_case_message("Failed %s:%d", __PRETTY_FUNCTION__, __LINE__);
+            result = false;
+            break;
+        }
+        pid = Sys::launch("/app/flash/launchslave",exec_path,arg_buff,\
+                          Sys::LAUNCH_OPTIONS_UNIQUE_NAMES, ram_size ,0,0);
+        if(pid < 0){
+            print_case_message("Failed %s:%d", __PRETTY_FUNCTION__, __LINE__);
+            result = false;
+            break;
+        }
+        //print_case_message("LAUNCH_OPTIONS_UNIQUE_NAMES %d",pid);
+        Timer::wait_microseconds(40000);
+        if(Sys::free_ram("/app/flash/launchslave",0)!=0){
+            print_case_message("Failed %s:%d", __PRETTY_FUNCTION__, __LINE__);
+            result = false;
+            break;
+        }
         //stop apps
     }
     return result;
-
 }
 
 /*@brief performsnce test for sys/Sys use "api-sys-test -sys -performance"
@@ -69,4 +218,10 @@ bool SysTest::execute_class_performance_case(){
 
     return result;
 
+}
+static void rand_string_value(u16 size,String & string){
+    string.free();
+    for (u16 i =0;i<size;i++){
+        string.append(((u8)rand()&0xff));
+    }
 }
