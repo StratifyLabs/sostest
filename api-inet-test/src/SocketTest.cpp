dd9dcc257ed9ce8e6266aa045d77244e5b63c4b1
Tyler Gilbert <tyler.w.gilbert@gmail.com>
2018-10-11 20:28:43 -0600

    Updated sockettest

--- api-inet-test/src/SocketTest.cpp
+++ api-inet-test/src/SocketTest.cpp
@@ -24,26 +24,58 @@ bool SocketTest::execute_class_api_case(){
 
 bool SocketTest::execute_socket_case(){
 
-
-	Vector<SocketAddressInfo> list = SocketAddressInfo().fetch_node("stratifylabs.co");
+	Vector<SocketAddressInfo> list = SocketAddressInfo().fetch_node("localhost");
 
 	if( list.count() == 0 ){
 		print_case_failed("Failed to fetch node");
 		return case_result();
 	}
 
-	Socket socket;
-	SocketAddress address(list.at(0), 80);
+	Socket local_host_listen_socket;
+	SocketAddress localhost_address(list.at(0), 8080);
 
-
-	print_case_message("create socket at %s", address.address_to_string().str());
-	if( socket.create(address) < 0 ){
+	print_case_message("create socket at %s", localhost_address.address_to_string().str());
+	if( local_host_listen_socket.create(localhost_address) < 0 ){
 		print_case_failed("failed to create socket");
 		return case_result();
 	}
 
+	//listen on port 80 of localhost
+	local_host_listen_socket.clear_error_number();
+	local_host_listen_socket << SocketOption().reuse_address() << SocketOption().reuse_port();
 
-	if( socket.close() < 0 ){
+	if( local_host_listen_socket.error_number() != 0 ){
+		print_case_failed("failed to set socket options (%d)", local_host_listen_socket.error_number());
+	}
+
+	//connect to port 80 of localhost
+	perror("Pre bind-error");
+	if( local_host_listen_socket.bind(localhost_address) < 0 ){
+		print_case_failed("Failed to bind to localhost (%d)", local_host_listen_socket.error_number());
+		perror("failed to bind");
+		return case_result();
+	}
+
+
+	if( local_host_listen_socket.listen() < 0 ){
+		print_case_failed("Failed to list on localhost");
+		return case_result();
+	}
+
+	print_case_message("Listening on localhost:%d", local_host_listen_socket.address().port());
+	//now accept -- this will block until a request arrives
+	Socket local_host_session_socket = local_host_listen_socket.accept();
+
+	Data incoming(1024);
+	int bytes_received = local_host_session_socket.read(incoming);
+
+	String reply;
+	reply = "Hello\n";
+	if( bytes_received > 0 ){
+		local_host_session_socket.write(reply);
+	}
+
+	if( local_host_listen_socket.close() < 0 ){
 		print_case_failed("failed to close socket");
 		return case_result();
 	}
